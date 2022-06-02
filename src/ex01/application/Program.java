eaa1a2f605a3e8c9272587b756fd92bccd08f003
Moises Silva <moiss.silva@outlook.com>
2022-06-02 17:50:09 -0300

    Exercise of Generics - Most Expensive List

--- src/ex01/application/Program.java
+++ src/ex01/application/Program.java
@@ -1,9 +1,40 @@
 package ex01.application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import ex01.entities.Product;
+import ex01.service.CalculationService;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		
+		List<Product> list = new ArrayList<>();
+		
+		String path = "/Users/moiss/Documents/Java/in1.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			String line = br.readLine();
+			
+			while(line!=null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0],Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+			
+			Product x = CalculationService.max(list);
+			System.out.print("Most expensive: ");
+			System.out.println(x);
+		}
+		catch(IOException e) {
+			System.out.println("Error: "+e.getMessage());
+		}
 
 	}
 
